0063c3619d65284f759f213ca3c2aa2599acb168
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2013-11-24 18:26:53 +0100

    Finished cell object

--- src/Cell.java
+++ src/Cell.java
@@ -1,41 +1,66 @@
 /**
+ * Class representing a cell in a spreadsheet table
  * 
  * @author m.olsthoorn
- *
  */
 public class Cell 
 {
 
 	/**
-	 * 
+	 * Variable containing the content of the cell
 	 */
 	private String content;
 	
+	/**
+	 * Constructor for the cell class
+	 * 
+	 * @param content String String containing the content of the cell 
+	 */
 	public Cell(String content) 
 	{
 		this.content = content;
 	}
 
 	public Cell() {
-		content = "";
+		this.content = "";
 	}
 
 	/**
+	 * Method that returns the content of the cell
 	 * 
-	 * @return
+	 * @return String Content of the cell
 	 */
 	public String getContent() 
 	{
-		return content;
+		return this.content;
 	}
 
 	/**
+	 * Method that sets the content of the cell
 	 * 
-	 * @param content
+	 * @param content String New content for the cell
 	 */
 	public void setContent(String content) 
 	{
 		this.content = content;
+	}
+	
+	public String toString()
+	{
+		return this.content;
+	}
+	
+	public boolean equals(Object other)
+	{
+		if (other instanceof Cell)
+		{
+			Cell that = (Cell) other; 
+			if (that.content.equals(this.content))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
