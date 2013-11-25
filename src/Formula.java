8d25dff3cc25396c64d372b19b6fe7bab10e8514
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2013-11-25 07:52:27 +0100

    modified:   src/Formula.java 	modified:   src/FormulaTest.java
    	-Added basic formula recognition and parsing

--- src/Formula.java
+++ src/Formula.java
@@ -11,7 +11,7 @@ public class Formula {
 	static Pattern plus = Pattern.compile("([0-9]+)\\s*\\+\\s*([0-9]+)");
 	static Pattern minus = Pattern.compile("([0-9]+)\\s*\\-\\s*([0-9]+)");
 	static Pattern times = Pattern.compile("([0-9]+)\\s*\\*\\s*([0-9]+)");
-	static Pattern devide = Pattern.compile("([0-9]+)\\s*\\/\\s*([0-9]+)");
+	static Pattern divide = Pattern.compile("([0-9]+)\\s*\\/\\s*([0-9]+)");
 
 	public static String parse(String formula)
 	{
@@ -26,6 +26,24 @@ public class Formula {
 			if (m.find())
 			{
 				res = Integer.toString((Integer.parseInt(m.group(1)) + Integer.parseInt(m.group(2))));
+			}
+			
+			Matcher m2 = minus.matcher(res);
+			if (m2.find())
+			{
+				res = Integer.toString((Integer.parseInt(m2.group(1)) - Integer.parseInt(m2.group(2))));
+			}
+			
+			Matcher m3 = times.matcher(res);
+			if (m3.find())
+			{
+				res = Integer.toString((Integer.parseInt(m3.group(1)) * Integer.parseInt(m3.group(2))));
+			}
+			
+			Matcher m4 = divide.matcher(res);
+			if (m4.find())
+			{
+				res = Integer.toString((Integer.parseInt(m4.group(1)) / Integer.parseInt(m4.group(2))));
 			}
 		} else
 		{
--- src/FormulaTest.java
+++ src/FormulaTest.java
@@ -22,5 +22,59 @@ public class FormulaTest {
 		String i = Formula.parse("=6	+	9");
 		assertEquals("15", i);
 	}
+	
+	@Test
+	public void testMinus() {
+		String i = Formula.parse("=20 - 9");
+		assertEquals("11", i);
+	}
+	
+	@Test
+	public void testMinus2() {
+		String i = Formula.parse("=20-9");
+		assertEquals("11", i);
+	}
+	
+	@Test
+	public void testMinus3() {
+		String i = Formula.parse("=20	-	9");
+		assertEquals("11", i);
+	}
+	
+	@Test
+	public void testTimes() {
+		String i = Formula.parse("=4 * 5");
+		assertEquals("20", i);
+	}
+	
+	@Test
+	public void testTimes2() {
+		String i = Formula.parse("=4*5");
+		assertEquals("20", i);
+	}
+	
+	@Test
+	public void testTimes3() {
+		String i = Formula.parse("=4	*	5");
+		assertEquals("20", i);
+	}
+	
+	@Test
+	public void testDivide() {
+		String i = Formula.parse("=100 / 5");
+		assertEquals("20", i);
+	}
+	
+	@Test
+	public void testDivide2() {
+		String i = Formula.parse("=100/5");
+		assertEquals("20", i);
+	}
+	
+	@Test
+	public void testDivide3() {
+		String i = Formula.parse("=100	/	5");
+		assertEquals("20", i);
+	}
 
 }
