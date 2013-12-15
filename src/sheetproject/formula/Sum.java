de278648092639e385dcbc52ffb9503817c5652f
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2013-12-15 18:17:48 +0100

    Update Sum.java

--- src/sheetproject/formula/Sum.java
+++ src/sheetproject/formula/Sum.java
@@ -1,109 +1,74 @@
 package sheetproject.formula;
 
-import java.io.FileNotFoundException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import sheetproject.exception.IllegalFormulaException;
 
+import sheetproject.alfabet.Alfabet;
+import sheetproject.exception.CharacterOutOfBoundsException;
+import sheetproject.exception.IllegalFormulaException;
 import sheetproject.spreadsheet.Sheet;
 
-public class Parser {
-	
-	static Pattern format = Pattern.compile("(([A-Z]{2,10})\\((.*)\\))");
-	
-	private static String[] allowedFormulas = 
-	{
-		"Average","Count","Counta","Countif","If","Int","Islogical","Iseven",
-		"Isnumber","Lower","Max","Median","Min","Mod","Not","Or","Power",
-		"Product","Proper","Rounddown","Roundup","Sign","Sqrt","Sum","Sumif"
-	};
-	
-	public static String parse(String formula, Sheet data) throws IllegalFormulaException
-	{		
 
-		formula = formula.trim();
-		if (formula.startsWith("="))
-		{
-			 formula = evaluate(formula, data);
-		}
-		return formula;
-	}
-	
-	static String evaluate(String formula, Sheet data) throws IllegalFormulaException
-	{
-		Matcher m = format.matcher(formula);
-		String res = formula;
-		
-		if(m.find())
-		{
-			String classname = m.group(2);
-			classname = classname.toLowerCase();
-			classname = Character.toUpperCase(classname.charAt(0)) + classname.substring(1);
-			
-			if (containsFormula(classname))
-			{
-				try
-				{
-					classname = "sheetproject.formula." + classname;
-					Class c = Class.forName(classname);
-					Class[] methodParameters = new Class[2];
-					methodParameters[0] = String.class;
-					methodParameters[1] = Sheet.class;
-					Object[] parameters = new Object[2];
-					parameters[0] = formula;
-					parameters[1] = data;
-					Method method = c.getMethod("evaluate", methodParameters);
-					res = (String) method.invoke(null, parameters);
-				}					
-				catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (NoSuchMethodException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			else
-			{
-				throw new IllegalFormulaException();
-			}
-		}
-		return res;
-	}
-	
-	public static boolean containsFormula(String formula)
-	{
-		for (String formulaName : allowedFormulas)
-		{
-			if (formula.equals(formulaName))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public static void main(String[] args) throws IllegalFormulaException
-	{
-		Sheet sheet = new Sheet();
-		String a = Parser.parse("=SUM(SUM(12,3),3)", sheet);
-		System.out.println(a);
-	}
-	
+public class Sum
+{
+
+        static Pattern sum = Pattern.compile("\\s*SUM\\(\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*,\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*\\)\\s*");
+        static Pattern sum2 = Pattern.compile("\\s*([A-Z]{1,2})([0-9]{1,6})\\s*");
+        
+        public static String evaluate(String formula, Sheet data) throws CharacterOutOfBoundsException, IllegalFormulaException 
+        {
+                String res = "";
+                
+                Matcher m = sum.matcher(formula);
+                if (m.find())
+                {
+                        String group1 = m.group(1);
+                        group1 = Parser.evaluate(group1, data);
+                        String group2 = m.group(2);
+                        group2 = Parser.evaluate(group2, data);
+                        
+                        Matcher m2 = sum2.matcher(group1);
+                        Matcher m3 = sum2.matcher(group2);
+                        
+                        int number1 = 0;
+                        try
+                        {
+                                number1 = Integer.parseInt(group1);
+                        }
+                        catch(Exception e)
+                        {
+                                
+                        }
+                        if (m2.find())
+                        {
+                                int i = Alfabet.parseChar(m.group(1));
+                                int j = Integer.parseInt(m.group(2));
+                                String cellContent = data.getCell(i, j).getFormula();
+                                cellContent = Parser.evaluate(cellContent, data);
+                                number1 = Integer.parseInt(cellContent);
+                        }
+                        
+                        int number2 = 0;
+                        try
+                        {
+                                number2 = Integer.parseInt(group2);
+                        }
+                        catch(Exception e)
+                        {
+                                
+                        }
+                        if (m3.find())
+                        {
+                                int i = Alfabet.parseChar(m.group(1));
+                                int j = Integer.parseInt(m.group(2));
+                                String cellContent = data.getCell(i, j).getFormula();
+                                cellContent = Parser.evaluate(cellContent, data);
+                                number2 = Integer.parseInt(cellContent);
+                        }
+                        
+                        res = Integer.toString(number1 + number2);
+                        
+                }
+                return res;
+        }
 }
