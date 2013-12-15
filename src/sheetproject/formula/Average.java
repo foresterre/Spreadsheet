851ab8ffe05ad594e1492aaf2a82b07ad966dc80
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2013-12-15 19:31:17 +0100

    Removed Extra Redundencies

--- src/sheetproject/formula/Average.java
+++ src/sheetproject/formula/Average.java
@@ -2,8 +2,6 @@ package sheetproject.formula;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import sheetproject.alfabet.Alfabet;
 import sheetproject.exception.CharacterOutOfBoundsException;
 import sheetproject.exception.IllegalFormulaException;
 import sheetproject.spreadsheet.Sheet;
@@ -13,7 +11,6 @@ public class Average
 {
 
         static Pattern sum = Pattern.compile("\\s*AVERAGE\\(\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*,\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*\\)\\s*");
-        static Pattern sum2 = Pattern.compile("\\s*([A-Z]{1,2})([0-9]{1,6})\\s*");
         
         public static String evaluate(String formula, Sheet data) throws CharacterOutOfBoundsException, IllegalFormulaException 
         {
@@ -26,10 +23,7 @@ public class Average
                         group1 = Parser.evaluate(group1, data);
                         String group2 = m.group(2);
                         group2 = Parser.evaluate(group2, data);
-                        
-                        Matcher m2 = sum2.matcher(group1);
-                        Matcher m3 = sum2.matcher(group2);
-                        
+                                                
                         int number1 = 0;
                         try
                         {
@@ -38,17 +32,6 @@ public class Average
                         catch(Exception e)
                         {
                                 
-                        }
-                        if (m2.find())
-                        {
-                        	String a = m2.group(0);
-                        	String ab = m2.group(1);
-                        	String abc = m2.group(2);
-                                int i = Alfabet.parseChar(m2.group(1));
-                                int j = Integer.parseInt(m2.group(2));
-                                String cellContent = data.getCell(i, j).getFormula();
-                                cellContent = Parser.evaluate(cellContent, data);
-                                number1 = Integer.parseInt(cellContent);
                         }
                         
                         int number2 = 0;
@@ -60,17 +43,8 @@ public class Average
                         {
                                 
                         }
-                        if (m3.find())
-                        {
-                                int i = Alfabet.parseChar(m3.group(1));
-                                int j = Integer.parseInt(m3.group(2));
-                                String cellContent = data.getCell(i, j).getFormula();
-                                cellContent = Parser.evaluate(cellContent, data);
-                                number2 = Integer.parseInt(cellContent);
-                        }
                         
-                        res = Integer.toString(number1 + number2);
-                        
+                        res = Integer.toString(number1 + number2);     
                 }
                 return res;
         }
--- src/sheetproject/formula/Sum.java
+++ src/sheetproject/formula/Sum.java
@@ -2,8 +2,6 @@ package sheetproject.formula;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import sheetproject.alfabet.Alfabet;
 import sheetproject.exception.CharacterOutOfBoundsException;
 import sheetproject.exception.IllegalFormulaException;
 import sheetproject.spreadsheet.Sheet;
@@ -13,7 +11,6 @@ public class Sum
 {
 
         static Pattern sum = Pattern.compile("\\s*SUM\\(\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*,\\s*([0-9]+|[A-Z][0-9]{1,6}|[A-Z]{2,10}\\(.*\\))\\s*\\)\\s*");
-        static Pattern sum2 = Pattern.compile("\\s*([A-Z]{1,2})([0-9]{1,6})\\s*");
         
         public static String evaluate(String formula, Sheet data) throws CharacterOutOfBoundsException, IllegalFormulaException 
         {
@@ -26,9 +23,6 @@ public class Sum
                         group1 = Parser.evaluate(group1, data);
                         String group2 = m.group(2);
                         group2 = Parser.evaluate(group2, data);
-                        
-                        Matcher m2 = sum2.matcher(group1);
-                        Matcher m3 = sum2.matcher(group2);
                         
                         int number1 = 0;
                         try
@@ -50,8 +44,7 @@ public class Sum
                                 
                         }
                         
-                        res = Integer.toString(number1 + number2);
-                        
+                        res = Integer.toString(number1 + number2);      
                 }
                 return res;
         }
