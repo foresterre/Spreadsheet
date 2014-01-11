253793ad5b3378f587b9685ed1dbae6e74c265ac
Martijn Gribnau <m.m.w.gribnau@student.tudelft.nl>
2014-01-11 21:33:31 +0100

    In progress update

---
+++ testfile.py
@@ -0,0 +1,21 @@
+'''
+Using jython 2.5.3
+Which uses python 2 syntax
+
+Team Scarlett Spreadsheet
+'''
+
+print 'Hello World!'
+
+no_space = 4<5
+spaces = 5 < 8
+print no_space;
+print spaces;
+if no_space == True:
+	pass
+
+if spaces == True:
+	pass
+
+
+
