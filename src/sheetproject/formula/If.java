709ba6c45324457bb5d0cbdade29e9e01a1fca70
Martijn Gribnau <m.m.w.gribnau@student.tudelft.nl>
2014-01-12 15:36:35 +0100

    The other way around

--- src/sheetproject/formula/If.java
+++ src/sheetproject/formula/If.java
@@ -13,53 +13,113 @@ import sheetproject.exception.NullObjectException;
 import sheetproject.spreadsheet.Cell;
 import sheetproject.spreadsheet.Sheet;
 
-/* BEFORE BUILD:
- * Add jython to the classpath
- * java -cp "./;jython-2.5.3.jar" jythonEx
- * OR Eclipse -> Alt+Enter with focus on Spreadsheet( =Project properties) -> Java Build Path -> Add Jar -> jython-2.5.3.jar
- * 
- * 
- */
+
 
 public class If
-{
-	// TODO: Working it out
-	// Eval staat standaard uit in jvm
-	// http://stackoverflow.com/questions/1016128/where-is-java-scripting-engine-used
-	// http://learnpythonthehardway.org/book/ex27.html
-	// http://docs.python.org/2/
-	// http://docs.oracle.com/javase/6/docs/api/javax/script/ScriptEngineManager.html#getEngineByName%28java.lang.String%29
-  	
+{ 	
 	// =IF(CONDITION, IF_TRUE, IF_FALSE)
 	
-	
-	private static String condition;
 	private static boolean evalResult = false;
+	
+	
 	static Pattern formulaPattern = Pattern.compile("\\s*IF\\(\\s*([0-9]+|[0-9]+\\.[0-9]+|[A-Z]{1,2}[0-9]{1,6}|[A-Z]{2,10}\\(.*\\)|[a-zA-Z0-9\\s=]+)\\s*,\\s*([a-zA-Z\\s0-9]+)\\s*,\\s*([a-zA-Z\\s0-9]+)\\s*\\)\\s*");
 	
-  	
+	static String keep[];
+	public static void evalConditionJ(String cond)
+	{
+		
+		// Not sure if I'm going to use || or else if's
+		if(cond.contains("<") )
+		{
+			// TODO: less than
+			// REGEX voor <
+			keep = cond.split("__REGEX__");
+			
+			double val1 = Double.parseDouble(keep[0]);
+			double val2 = Double.parseDouble(keep[2]);
+			
+			if (val1 < val2)
+			{
+				evalResult = true;
+			}
+			
+					
+		}
+		else if(cond.contains(">"))
+		{
+			// TODO: more than
+			keep = cond.split("__REGEX__");
+			
+			double val1 = Double.parseDouble(keep[0]);
+			double val2 = Double.parseDouble(keep[2]);
+			
+			if (val1 > val2)
+			{
+				evalResult = true;
+			}
+		}
+		else if(cond.contains("="))
+		{
+			// TODO: equal
+			keep = cond.split("__REGEX__");
+			
+			double val1 = Double.parseDouble(keep[0]);
+			double val2 = Double.parseDouble(keep[2]);
+			
+			if (val1 == val2)
+			{
+				evalResult = true;
+			}
+		}
+		else if(cond.contains("<="))
+		{
+			// TODO: less or equal
+			keep = cond.split("__REGEX__");
+			
+			double val1 = Double.parseDouble(keep[0]);
+			double val2 = Double.parseDouble(keep[2]);
+			
+			if (val1 <= val2)
+			{
+				evalResult = true;
+			}
+		}
+		else if(cond.contains(">="))
+		{
+			// TODO: more or equal
+			keep = cond.split("__REGEX__");
+			
+			double val1 = Double.parseDouble(keep[0]);
+			double val2 = Double.parseDouble(keep[2]);
+			
+			if (val1 >= val2)
+			{
+				evalResult = true;
+			}
+		}
+		else
+		{
+			// TODO: Unparseable
+			evalResult = false;
+		}
+	}
+	
 	public static String evaluate(String formula, Sheet data) throws CharacterOutOfBoundsException, IllegalFormulaException, ScriptException 
 	{
 		String res = "";
 		     
         Matcher m = formulaPattern.matcher(formula);
         
-        // Make sure the statement is parseable by python
-        evalConditionSplitter(formula);
-        
         if (m.find())
         {
                 	
         	String group1 = m.group(1);
         	group1 = Parser.evaluate(group1, data);
-        	condition = group1;
         	
         	String group2 = m.group(2);
         	String group3 = m.group(3);
             
-        	// Use jython as a python script engine
-        	ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
-            evalResult = (boolean) engine.eval(condition);
+        	evalConditionJ(group1);
         	
         	if (evalResult == true)
         	{
@@ -77,90 +137,148 @@ public class If
 	}
 	
 	/**
-	 * Avaible:
-	 * <, >, =, <=, =>
-	 * @param s
-	 */
-	public static void evalConditionSplitter(String s)
-	{
-		String[] keep;
-		
-		// Not sure if I'm going to use || or else if's
-		if(s.contains("<") || s.contains(">") || s.contains("=") || s.contains("<=") || s.contains(">="))
-		{
-			// TODO: less than
-			// REGEX voor <
-			keep = s.split("__REGEX__");
-			condition = "";
-					
-			// Replace = with pythons 'is'
-			condition = condition.replace("=", "is");
-		}
-		else if(s.contains(">"))
-		{
-			// TODO: more than
-			keep = s.split("");
-		}
-		else if(s.contains("="))
-		{
-			// TODO: equal
-			keep = s.split("");
-		}
-		else if(s.contains("<="))
-		{
-			// TODO: less or equal
-			keep = s.split("");
-		}
-		else if(s.contains(">="))
-		{
-			// TODO: more or equal
-			keep = s.split("");
-		}
-		else
-		{
-			// TODO: Unparseable
-			condition = "False";
-		}
-		
-	}
-	
-	/**
-	 * TEST
+	 * MAIN_TEST
 	 * @param args
 	 * @throws NullObjectException 
 	 * @throws IndexOutOfBoundsException 
 	 * @throws CharacterOutOfBoundsException 
 	 * @throws IllegalFormulaException 
 	 */
-	public static void main(String[] args) throws ScriptException, IndexOutOfBoundsException, NullObjectException, IllegalFormulaException, CharacterOutOfBoundsException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
-        engine.eval("import sys");
-        engine.eval("print sys");
-
+	public static void main(String[] args) throws ScriptException, IndexOutOfBoundsException, NullObjectException, IllegalFormulaException, CharacterOutOfBoundsException 
+	{
         
         
-        engine.put("a", 42);
-        engine.eval("print a");
-        engine.eval("x = 2 + 2");
-        engine.eval("y=4<5");
-        engine.eval("print y");
-        
-        
-        
-        Object x = engine.get("x");
-        System.out.println("x: " + x);
-        
-        
-        //new
-//        Sheet sheet = new Sheet();
-//		Cell a = new Cell("6");
-//		Cell b = new Cell("12");
-//		sheet.setCell(a, 1, 1);
-//		sheet.setCell(b, 1, 2);
-//		
-//		String z = Parser.parse("=SUM(6,PRODUCT(A1,A2))", sheet);
-//		
-//		String q = Parser.parse("=IF(A1=A2,A1,A2)", sheet);
-//		System.out.println(z);
+      Sheet sheet = new Sheet();
+		Cell a = new Cell("6");
+		Cell b = new Cell("12");
+		sheet.setCell(a, 1, 1);
+		sheet.setCell(b, 1, 2);
+		
+		
+		String q = Parser.parse("=IF(A1=A2,2,3)", sheet);
+		System.out.println(q);
     }
+	
+//  THE JYTHON WAY
+
+	/* BEFORE BUILD:
+	 * Add jython to the classpath
+	 * java -cp "./;jython-2.5.3.jar" jythonEx
+	 * OR Eclipse -> Alt+Enter with focus on Spreadsheet( =Project properties) -> Java Build Path -> Add Jar -> jython-2.5.3.jar
+	 * 
+	 * 
+	 */
+	
+// TODO: Making it work
+// Eval staat standaard uit in jvm
+// http://stackoverflow.com/questions/1016128/where-is-java-scripting-engine-used
+// http://learnpythonthehardway.org/book/ex27.html
+// http://docs.python.org/2/
+// http://docs.oracle.com/javase/6/docs/api/javax/script/ScriptEngineManager.html#getEngineByName%28java.lang.String%29
+
+//	public static String evaluate(String formula, Sheet data) throws CharacterOutOfBoundsException, IllegalFormulaException, ScriptException 
+//	{
+//		String res = "";
+//		     
+//        Matcher m = formulaPattern.matcher(formula);
+//        
+//        // Make sure the statement is parseable by python
+//        // evalConditionPy(formula);
+//        
+//        if (m.find())
+//        {
+//                	
+//        	String group1 = m.group(1);
+//        	group1 = Parser.evaluate(group1, data);
+//        	condition = group1;
+//        	
+//        	String group2 = m.group(2);
+//        	String group3 = m.group(3);
+//            
+//        	ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+//        	
+//        	//Make True, true and False, false
+//            evalResult = (boolean) engine.eval(condition);
+//        	
+//        	if (evalResult == true)
+//        	{
+//        		group2 = Parser.parse(group2, data);
+//        		return group2;
+//        	}
+//        	else
+//        	{
+//        		group3 = Parser.parse(group3, data);
+//        		return group3;
+//        	}
+//                       	
+//        }
+//        return res;
+//	}
+	
+//	
+//	/**
+//	 * Avaible:
+//	 * <, >, =, <=, =>
+//	 * @param s
+//	 */
+//	public static void evalConditionPy(String s)
+//	{
+//		String[] keep;
+//		
+//		// Not sure if I'm going to use || or else if's
+//		if(s.contains("<") || s.contains(">") || s.contains("=") || s.contains("<=") || s.contains(">="))
+//		{
+//			// TODO: less than
+//			// REGEX voor <
+//			keep = s.split("__REGEX__");
+//			condition = "";
+//					
+//			// Replace = with pythons 'is', so it's not being assigned
+//			condition = condition.replace("=", "is");
+//		}
+//		else if(s.contains(">"))
+//		{
+//			// TODO: more than
+//			keep = s.split("");
+//		}
+//		else if(s.contains("="))
+//		{
+//			// TODO: equal
+//			keep = s.split("");
+//		}
+//		else if(s.contains("<="))
+//		{
+//			// TODO: less or equal
+//			keep = s.split("");
+//		}
+//		else if(s.contains(">="))
+//		{
+//			// TODO: more or equal
+//			keep = s.split("");
+//		}
+//		else
+//		{
+//			// TODO: Unparseable
+//			condition = "False";
+//		}
+		
+	
+	
+// 	MAIN TEST	
+//  ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+//  engine.eval("import sys");
+//  engine.eval("print sys");
+//
+//  
+//  
+//  engine.put("a", 50);
+//  engine.eval("print a");
+//  engine.eval("x = 2 + 2");
+//  engine.eval("y=4<5");
+//  engine.eval("print y");
+//  
+//  
+//  
+//  Object x = engine.get("x");
+//  System.out.println("x: " + x);
 }
